e5c4837599629b4a34ce2fa458a3b4da5dab0b5f
Thiago Ribeiro <thiagitosouza@gmail.com>
2018-10-29 22:52:44 -0300

    fix context when input is 'A'

--- lib/diamond.rb
+++ lib/diamond.rb
@@ -1,6 +1,11 @@
 class Diamond
 
   def build(input)
-    puts input
+    base_bytes_number = string_bytes_number('A')
+    return "#{input}\n" if base_bytes_number - string_bytes_number(input) == 0
+  end
+
+  def string_bytes_number(string)
+    string.bytes.first
   end
 end
